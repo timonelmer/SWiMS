6d17b60086d83dc1e3fa8fa906a6bbc77a834e56
Timon Elmer <timon.elmer@gmail.com>
2024-11-25 09:39:19 +0100

    simplified handing of 0 values (indicating "seen, but unqouted", but with different meaings for checkboxes and scales)

--- 01_data_cleaning.R
+++ 01_data_cleaning.R
@@ -57,6 +57,8 @@ removed_institutions
 sum(removed_institutions$count)
 # --> total 41 observations removed because of fewer than 10 observations
 # --> total of 15 institutions affected
+dat <- dat[!(dat$institution %in% removed_institutions$institution),]
+
 
 # Remove responses before 2024-05-13 (the official start of the survey)
 official_start_date <- as.Date("2024-05-13")
@@ -150,100 +152,101 @@ ggplot(count_data, aes(x = zero_count)) +
        y = "Frequency") +
   theme_minimal()
 
-### TODO: if 0s are like NA's for specific variables then make NA's out of them (with the help of the codebook)
+
 ##  detangling strategy: Mutate all meaningful 0 to x and then mutate all other 0 (skipped questions) to an NA, then mutate the x back to 0
+# for all variables that do contain the level "quoted" and "not quoted", replace 0s with NAs
 
-# Mutate consent variables to change 1 to "x"
-dat <- dat %>%
-  mutate(
-    consent1 = if_else(consent1 == 0, "x", as.character(consent1)),
-    consent2 = if_else(consent2 == 0, "x", as.character(consent2)),
-    consent3 = if_else(consent3 == 0, "x", as.character(consent3))
-  )
-
-# Mutate gender variables to change 0 to "x"
-dat <- dat %>%
-  mutate(
-    female = if_else(female == 0, "x", as.character(female)),
-    male = if_else(male == 0, "x", as.character(male)),
-    nonbinary = if_else(nonbinary == 0, "x", as.character(nonbinary)),
-    genderNA = if_else(genderNA == 0, "x", as.character(genderNA))
-  )
-
-# Mutate care variables to change 0 to "x"
-dat <- dat %>%
-  mutate(
-    careKids = if_else(careKids == 0, "x", as.character(careKids)),
-    careFamily = if_else(careFamily == 0, "x", as.character(careFamily)),
-    careNo = if_else(careNo == 0, "x", as.character(careNo))
-  )
-
-# Mutate addhours variables to change 0 to "x"
-dat <- dat %>%
-  mutate(
-    researchwitharAddhours = if_else(researchwitharAddhours == 0, "x", as.character(researchwitharAddhours)),
-    researchwithoutarAddhours = if_else(researchwithoutarAddhours == 0, "x", as.character(researchwithoutarAddhours)),
-    fundingapplicAddhours = if_else(fundingapplicAddhours == 0, "x", as.character(fundingapplicAddhours)),
-    teachingAddhours = if_else(teachingAddhours == 0, "x", as.character(teachingAddhours)),
-    supervisingAddhours = if_else(supervisingAddhours == 0, "x", as.character(supervisingAddhours)),
-    adminAddhours = if_else(adminAddhours == 0, "x", as.character(adminAddhours)),
-    politicalworkAddhours = if_else(politicalworkAddhours == 0, "x", as.character(politicalworkAddhours))
-  )
-
-# Mutate discrimination variables to change 0 to "x"
-dat <- dat %>%
-  mutate(
-    discrAge = if_else(discrAge == 0, "x", as.character(discrAge)),
-    discrSES = if_else(discrSES == 0, "x", as.character(discrSES)),
-    discrDisability = if_else(discrDisability == 0, "x", as.character(discrDisability)),
-    discrSex = if_else(discrSex == 0, "x", as.character(discrSex)),
-    discrGender = if_else(discrGender == 0, "x", as.character(discrGender)),
-    discrSexualorientation = if_else(discrSexualorientation == 0, "x", as.character(discrSexualorientation)),
-    discrPhysical = if_else(discrPhysical == 0, "x", as.character(discrPhysical)),
-    discrLanguage = if_else(discrLanguage == 0, "x", as.character(discrLanguage)),
-    discrNationality = if_else(discrNationality == 0, "x", as.character(discrNationality)),
-    discrEthnicity = if_else(discrEthnicity == 0, "x", as.character(discrEthnicity)),
-    discrReligion = if_else(discrReligion == 0, "x", as.character(discrReligion)),
-    discrCompetency = if_else(discrCompetency == 0, "x", as.character(discrCompetency)),
-    discrHierarchy = if_else(discrHierarchy == 0, "x", as.character(discrHierarchy)),
-    discrUnknown = if_else(discrUnknown == 0, "x", as.character(discrUnknown)),
-    discrOther = if_else(discrOther == 0, "x", as.character(discrOther)),
-    discrNA = if_else(discrNA == 0, "x", as.character(discrNA)),
-    discrfundingSNF = if_else(discrfundingSNF == 0, "x", as.character(discrfundingSNF)),
-    discrfundingnonSNF = if_else(discrfundingnonSNF == 0, "x", as.character(discrfundingnonSNF)),
-    discrfundingUniversity = if_else(discrfundingUniversity == 0, "x", as.character(discrfundingUniversity)),
-    discrother1 = if_else(discrother1 == 0, "x", as.character(discrother1)),
-    discrother2 = if_else(discrother2 == 0, "x", as.character(discrother2)),
-    dupl1_discrUnknown = if_else(dupl1_discrUnknown == 0, "x", as.character(dupl1_discrUnknown)),
-    dupl1_discrNA = if_else(dupl1_discrNA == 0, "x", as.character(dupl1_discrNA))
-  )
-
-# Mutate pressure variables to change 0 to "x"
-dat <- dat %>%
-  mutate(
-    pressureqrpFunder = if_else(pressureqrpFunder == 0, "x", as.character(pressureqrpFunder)),
-    pressureqrpStakeholder = if_else(pressureqrpStakeholder == 0, "x", as.character(pressureqrpStakeholder)),
-    pressureqrpSupervisor = if_else(pressureqrpSupervisor == 0, "x", as.character(pressureqrpSupervisor)),
-    pressureqrpCollegueIn = if_else(pressureqrpCollegueIn == 0, "x", as.character(pressureqrpCollegueIn)),
-    pressureqrpManagerIn = if_else(pressureqrpManagerIn == 0, "x", as.character(pressureqrpManagerIn)),
-    pressureqrpCollegueOut = if_else(pressureqrpCollegueOut == 0, "x", as.character(pressureqrpCollegueOut)),
-    pressureqrpManagerOut = if_else(pressureqrpManagerOut == 0, "x", as.character(pressureqrpManagerOut)),
-    pressureqrpCompetitiveenviron = if_else(pressureqrpCompetitiveenviron == 0, "x", as.character(pressureqrpCompetitiveenviron)),
-    pressureqrpMyself = if_else(pressureqrpMyself == 0, "x", as.character(pressureqrpMyself)),
-    pressureqrpOther1 = if_else(pressureqrpOther1 == 0, "x", as.character(pressureqrpOther1)),
-    pressureqrpOther2 = if_else(pressureqrpOther2 == 0, "x", as.character(pressureqrpOther2)),
-    pressureNo = if_else(pressureNo == 0, "x", as.character(pressureNo))
-  )
-
-# Mutate institution variable to change 0 to "x"
+# # Mutate consent variables to change 1 to "x"
 # dat <- dat %>%
 #   mutate(
-#     institution = if_else(institution == 0, "x", as.character(institution))
+#     consent1 = if_else(consent1 == 0, "x", as.character(consent1)),
+#     consent2 = if_else(consent2 == 0, "x", as.character(consent2)),
+#     consent3 = if_else(consent3 == 0, "x", as.character(consent3))
 #   )
 # 
-# Replace 0s with NA in all other variables where 0 indicates a skipped question
-dat <- dat %>%
-  mutate(across(everything(), ~ if_else(. == 0, NA, .)))
+# # Mutate gender variables to change 0 to "x"
+# dat <- dat %>%
+#   mutate(
+#     female = if_else(female == 0, "x", as.character(female)),
+#     male = if_else(male == 0, "x", as.character(male)),
+#     nonbinary = if_else(nonbinary == 0, "x", as.character(nonbinary)),
+#     genderNA = if_else(genderNA == 0, "x", as.character(genderNA))
+#   )
+# 
+# # Mutate care variables to change 0 to "x"
+# dat <- dat %>%
+#   mutate(
+#     careKids = if_else(careKids == 0, "x", as.character(careKids)),
+#     careFamily = if_else(careFamily == 0, "x", as.character(careFamily)),
+#     careNo = if_else(careNo == 0, "x", as.character(careNo))
+#   )
+# 
+# # Mutate addhours variables to change 0 to "x"
+# dat <- dat %>%
+#   mutate(
+#     researchwitharAddhours = if_else(researchwitharAddhours == 0, "x", as.character(researchwitharAddhours)),
+#     researchwithoutarAddhours = if_else(researchwithoutarAddhours == 0, "x", as.character(researchwithoutarAddhours)),
+#     fundingapplicAddhours = if_else(fundingapplicAddhours == 0, "x", as.character(fundingapplicAddhours)),
+#     teachingAddhours = if_else(teachingAddhours == 0, "x", as.character(teachingAddhours)),
+#     supervisingAddhours = if_else(supervisingAddhours == 0, "x", as.character(supervisingAddhours)),
+#     adminAddhours = if_else(adminAddhours == 0, "x", as.character(adminAddhours)),
+#     politicalworkAddhours = if_else(politicalworkAddhours == 0, "x", as.character(politicalworkAddhours))
+#   )
+# 
+# # Mutate discrimination variables to change 0 to "x"
+# dat <- dat %>%
+#   mutate(
+#     discrAge = if_else(discrAge == 0, "x", as.character(discrAge)),
+#     discrSES = if_else(discrSES == 0, "x", as.character(discrSES)),
+#     discrDisability = if_else(discrDisability == 0, "x", as.character(discrDisability)),
+#     discrSex = if_else(discrSex == 0, "x", as.character(discrSex)),
+#     discrGender = if_else(discrGender == 0, "x", as.character(discrGender)),
+#     discrSexualorientation = if_else(discrSexualorientation == 0, "x", as.character(discrSexualorientation)),
+#     discrPhysical = if_else(discrPhysical == 0, "x", as.character(discrPhysical)),
+#     discrLanguage = if_else(discrLanguage == 0, "x", as.character(discrLanguage)),
+#     discrNationality = if_else(discrNationality == 0, "x", as.character(discrNationality)),
+#     discrEthnicity = if_else(discrEthnicity == 0, "x", as.character(discrEthnicity)),
+#     discrReligion = if_else(discrReligion == 0, "x", as.character(discrReligion)),
+#     discrCompetency = if_else(discrCompetency == 0, "x", as.character(discrCompetency)),
+#     discrHierarchy = if_else(discrHierarchy == 0, "x", as.character(discrHierarchy)),
+#     discrUnknown = if_else(discrUnknown == 0, "x", as.character(discrUnknown)),
+#     discrOther = if_else(discrOther == 0, "x", as.character(discrOther)),
+#     discrNA = if_else(discrNA == 0, "x", as.character(discrNA)),
+#     discrfundingSNF = if_else(discrfundingSNF == 0, "x", as.character(discrfundingSNF)),
+#     discrfundingnonSNF = if_else(discrfundingnonSNF == 0, "x", as.character(discrfundingnonSNF)),
+#     discrfundingUniversity = if_else(discrfundingUniversity == 0, "x", as.character(discrfundingUniversity)),
+#     discrother1 = if_else(discrother1 == 0, "x", as.character(discrother1)),
+#     discrother2 = if_else(discrother2 == 0, "x", as.character(discrother2)),
+#     dupl1_discrUnknown = if_else(dupl1_discrUnknown == 0, "x", as.character(dupl1_discrUnknown)),
+#     dupl1_discrNA = if_else(dupl1_discrNA == 0, "x", as.character(dupl1_discrNA))
+#   )
+# 
+# # Mutate pressure variables to change 0 to "x"
+# dat <- dat %>%
+#   mutate(
+#     pressureqrpFunder = if_else(pressureqrpFunder == 0, "x", as.character(pressureqrpFunder)),
+#     pressureqrpStakeholder = if_else(pressureqrpStakeholder == 0, "x", as.character(pressureqrpStakeholder)),
+#     pressureqrpSupervisor = if_else(pressureqrpSupervisor == 0, "x", as.character(pressureqrpSupervisor)),
+#     pressureqrpCollegueIn = if_else(pressureqrpCollegueIn == 0, "x", as.character(pressureqrpCollegueIn)),
+#     pressureqrpManagerIn = if_else(pressureqrpManagerIn == 0, "x", as.character(pressureqrpManagerIn)),
+#     pressureqrpCollegueOut = if_else(pressureqrpCollegueOut == 0, "x", as.character(pressureqrpCollegueOut)),
+#     pressureqrpManagerOut = if_else(pressureqrpManagerOut == 0, "x", as.character(pressureqrpManagerOut)),
+#     pressureqrpCompetitiveenviron = if_else(pressureqrpCompetitiveenviron == 0, "x", as.character(pressureqrpCompetitiveenviron)),
+#     pressureqrpMyself = if_else(pressureqrpMyself == 0, "x", as.character(pressureqrpMyself)),
+#     pressureqrpOther1 = if_else(pressureqrpOther1 == 0, "x", as.character(pressureqrpOther1)),
+#     pressureqrpOther2 = if_else(pressureqrpOther2 == 0, "x", as.character(pressureqrpOther2)),
+#     pressureNo = if_else(pressureNo == 0, "x", as.character(pressureNo))
+#   )
+# 
+# # Mutate institution variable to change 0 to "x"
+# # dat <- dat %>%
+# #   mutate(
+# #     institution = if_else(institution == 0, "x", as.character(institution))
+# #   )
+# # 
+# # Replace 0s with NA in all other variables where 0 indicates a skipped question
+# dat <- dat %>%
+#   mutate(across(everything(), ~ if_else(. == 0, NA, .)))
 
 # # Convert all columns with "x" factors to 0 in numeric form
 # dat <- dat %>%
@@ -256,6 +259,31 @@ dat <- dat %>%
 #     as.numeric(.)
 #   }))
 
+# Identify columns that contain levels "quoted" and "not quoted"
+columns_to_update <- sapply(dat, function(col) {
+  is.factor(col) && !(all(c("quoted", "not quoted") %in% levels(col)))
+})
+
+# Replace 0s with NA in the identified columns
+dat[columns_to_update] <- lapply(dat[columns_to_update], function(col) {
+  levels(col)[levels(col) == "0"] <- NA     # Replace level "0" with NA
+  col[col == "0"] <- NA                     # Replace values "0" with NA
+  droplevels(col)                         # Drop the unused "0" level
+})
+
+
+# Create the new 'gender' variable
+dat <- dat %>%
+  mutate(gender = case_when(
+    male == "quoted" & female == "not quoted" & nonbinary == "not quoted" ~ "male",
+    female == "quoted" & male == "not quoted" & nonbinary == "not quoted" ~ "female",
+    (nonbinary == "quoted") | (male == "quoted" & female == "quoted") | genderNA == "quoted" ~ "other",
+    TRUE ~ NA_character_  # Any other combinations are assigned as NA
+  ))
+
+## some codebook processing
+codebook <- openxlsx::read.xlsx("SWiMS_Codebook_v3.xlsx")
+codebook$question <- gsub(pattern = " \\(q_.*$", replacement = "", codebook$Item)
+
 ## save data as rdata object
-codebook <- openxlsx::read.xlsx("SWiMS_Codebook_v2.xlsx")
 save(dat, codebook, file = paste0("../data/SWiMS2024_Data_",Sys.Date(),".RData"))
